4886ec7ecb90e7ceef32133069b990fd141cbfe9
xieyalong <123456>
2019-11-25 10:34:45 +0800

    ------

--- shujuku/sqlalchemy_test/models.py
+++ shujuku/sqlalchemy_test/models.py
@@ -5,59 +5,73 @@ from sqlalchemy import Integer, String, Float, Boolean, DateTime, Text, INTEGER
 from sqlalchemy.ext.declarative import  declarative_base
 from sqlalchemy.testing.schema import Column
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+# pip install mysql-connector-python
+#'mysql+mysqlconnector://root:@127.0.0.1:3306/xyl_test',
+from  mysql import connector
 
-
-print('=====')
-
-#基类
-Base=declarative_base()
-class XylOrder(Base):
-    #映射数据表名
-    __tablename__ = 'xyl_order'
-    # id= Column(Integer,primary_key=True,foreign_key_ddl=1)
-    # String(200):长度200
-    # nullable：是否可以为null,如果可以为nul就不用写
-    #unique = True 是否唯一
-    # name=Column('order_name',String(200),nullable=False)
-    title = Column(String)
-    # age=Column(Integer)
-    # price = Column(Float)
-    # update_time=Column(DateTime,nullable=False)
-    # True=删除
-    # is_delete=Column(Boolean,nullable=False)
-
-
-
-print('-----------')
-#连接数据库
-# 'mysql://user:pwd@127.0.0.1:3306/omo_military'
-# mysql+pymysql：连接mysql，用pymysql驱动
-# root:用户名
-# pwd：密码，密码可以为null 写成'mysql://root:@127.0.0.1:3306/omo_military'
-# @localhost：主机地址 本地@127.0.0.1
-# omo_military：数据库
-
-
-# 创建数据库引擎，数据库的类名、账号、密码、登录方式、连接的数据库、数据库编码、是否显示回写
-#
-engine=create_engine('mysql+pymysql://root:@127.0.0.1:3306/xyl_test?charset=utf-8',
-                     # encoding='utf-8',
-                     # echo=True,
+engine=create_engine('mysql+mysqlconnector://root:@127.0.0.1:3306/xyl_test',
+                     encoding='utf-8',
+                     echo=True,
                      max_overflow=5,#允许在增加5个，一共是10个
                      pool_size=5,#连接池大小
                      pool_timeout=30,#池中没有连接等待的时间,否则报错
                      pool_recycle=-1#多久对线程池中的线程进行一次连接的回收，-1不回收，始终用原来那个
                      )
+conn = engine.connect()
+print('conn=',conn)
+result = conn.execute("select 1")
+print('======',result.fetchone())
+Base = declarative_base(engine)
+Session = sessionmaker(engine)
+session = Session()
 
-#创建继承Base的所有子类表
-Base.metadata.create_all(engine)
-#删除所有
-# Base.metadata.drop_all(engine)
 
 
+class User(Base):
+    __tablename__ = 'xyluser'  #表格名字
+    id = Column(Integer,primary_key=True,autoincrement=True)
+    name = Column(String(20),nullable=False)
+    title = Column(String(50))
 
+    # 自己封装查询方法
+    @classmethod
+    def get_all(cls):
+        return session.query(cls).all()
 
+    @classmethod
+    def get_id(cls,id):
+        return session.query(cls).filter_by(id=id).all()
 
+    @classmethod
+    def get_username(cls,username):
+        return session.query(cls).filter_by(username=username).all()
+
+    @property
+    def locked(self):
+        return self._locked
+
+    def __repr__(self):
+        return "<User(id='%s',username='%s',password='%s',creatime='%s',_locked='%s')>"%(
+            self.id,
+            self.username,
+            self.password,
+            self.creatime,
+            self._locked
+        )
+if __name__=='__main__':
+    # 验证是否成功可以在尾端进行如下操作：
+    # print('base=',dir(Base))
+    # print('session=',dir(session))
+    # Base.metadata.create_all()
+
+    # 验证
+    # add_user()
+    # delete_user()
+    # update_user()
+    # query_user()
+    print('=========')
+    print(User.get_id(1))  # 对上页封装的查询进行验证
 
 
 
