9860262d844d72a9e0c9c73fe24647878835b83d
xieyalong <123456>
2019-11-25 09:41:57 +0800

    ------

--- shujuku/sqlobject_test/PhoneNumber.py
+++
@@ -1,8 +0,0 @@
-import sqlobject
-
-class PhoneNumber(sqlobject.SQLObject):
-    _connection = conn
-    number = sqlobject.StringCol(length=14, unique=True)
-    owner = sqlobject.StringCol(length=255)
-    lastCall = sqlobject.DateTimeCol(default=None)
-PhoneNumber.createTable(ifNotExists=True)
