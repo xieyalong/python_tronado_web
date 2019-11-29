e267f67d1f15f7bfefce6a915ae4cb7d96adcb6f
xieyalong <123456>
2019-11-29 23:24:20 +0800

    ----

--- 其他/xlsx基础评估数据处理.py
+++ 其他/xlsx基础评估数据处理.py
@@ -26,7 +26,7 @@ def sheetFun(sheet):
 
 if __name__ == '__main__':
     # 加载excel文件
-    workbook = load_workbook(r'C:\test\数据.xlsx')
+    workbook = load_workbook(r'C:\test\初筛表录入内容.xlsm')
     # 获取所有sheet的名称
     sheets = workbook.worksheets
     print('sheet文件=',sheets)
@@ -39,5 +39,6 @@ if __name__ == '__main__':
 # 角色编号 1=士兵，2=医务并，3=军，4=旅，5=营或团，6=连，7=排，8=班
 # 这些时分秒 要写成 114100  001974  这样写
 # 性别写成 1=男 2=女 0=未知
+#pid：比如
 
 
