782cd208118a83fd31075c4ff8e2418d1021fefa
xieyalong <123456>
2019-12-14 17:55:55 +0800

    ------

--- 其他/用户信息/user_xlsx.py
+++ 其他/用户信息/user_xlsx.py
@@ -5,41 +5,61 @@ from openpyxl import load_workbook
 import  config
 import  json
 
-#解析分组
 def xlsx(sheet):
-    i = 0
     # print('sheet名称=', sheet)
     # print('根据坐标获取=',sheet['A1'].value,sheet['B1'].value)
     # print('根据坐标获取=', sheet['A2'].value, sheet['B2'].value)
 
     rows = sheet.rows
     # print(rows)
-    i = 0
+    title = []
+    listData=[]
+    index = 0
     # 迭代所有的行
     for row in rows:
         # print(row)
         # 获取整行数据，返回list
         line = [col.value for col in row]
-        print(line)
-        #第一行是标题
-        if 0 == i:
-            print('title=', line)
-        else:
-            print('value=', line)
-        i = i + 1
+        # print(line)
 
+        #第一行是标题
+        if 0 == index:
+            i = 0
+            for i in range(len(line)):
+                title.append(line[i])
+            # print('title=',title)
+        else:
+            i = 0
+            map={}
+            for i in range(len(line)):
+                str_title=title[i]
+                map[str_title]=line[i]
+                # print(str_title,line[i])
+            print('---------------------------------------------')
+            print('map',map)
+            listData.append(map)
+
+        index = index + 1
+    print('------------------listData---------------------------')
+    print('listData=',listData)
+    # print(listData)
+    # data=json.dumps(listData,ensure_ascii=False)
+    # print(data)
+    # path=config.base_dirs+'\\其他\\基础评估\\omo_stat_groupname.json'
+    # with open(path, 'w', encoding="utf-8") as f:
+    #     f.write(data)
+    # print('组=',data)
+    print('---------------------------------------------')
+    return listData
 
 if __name__ == '__main__':
-    path=config.base_dirs+'\\其他\\用户信息\\用户数据.xlsx'
+    path=config.base_dirs+'\\其他\\用户信息\\处理.xlsx'
     print(path)
     # 加载excel文件
     workbook = load_workbook(path)
     # 获取所有sheet的名称
     sheets = workbook.worksheets
     print('sheet文件=',sheets)
-    # zu=fenzu(sheets[0])
-    # ti=jiexiti(sheets[1])
-    # xzx(sheets[2])
     for sheet in sheets:
         xlsx(sheet)
 
